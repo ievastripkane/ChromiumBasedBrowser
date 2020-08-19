215860aa7a009a13033fee3e917544888c82bae5
Ieva Stripkāne <ieva.stripkane.2002@gmail.com>
2020-08-19 15:06:01 +0300

    version 15:05

--- Form1.cs
+++ Form1.cs
@@ -20,26 +20,26 @@ namespace ChromiumBasedBrowser
         {
             InitializeComponent();
             InitializeBrowser();
+            InitializeForm();
+        }
+
+        private void InitializeForm()
+        {
+            BrowserTabs.Height = ClientRectangle.Height - 25;
         }
 
         private void InitializeBrowser()
         {
             Cef.Initialize(new CefSettings());
-            browser = new ChromiumWebBrowser("https://datorium.eu");
+            browser = new ChromiumWebBrowser("https://google.lv");
             browser.Dock = DockStyle.Fill;
-            this.Controls.Add(browser);
+            BrowserTabs.TabPages[0].Controls.Add(browser);
+            browser.AddressChanged += Browser_AddressChanged;
         }
 
         private void toolStripButtonGo_Click(object sender, EventArgs e)
         {
-            try
-            {
-                browser.Load(toolStripAddressBar.Text);
-            }
-            catch
-            {
-
-            }
+            Navigate(toolStripAddressBar.Text);
         }
 
         private void toolStripButtonBack_Click(object sender, EventArgs e)
@@ -50,6 +50,46 @@ namespace ChromiumBasedBrowser
         private void toolStripButtonForward_Click(object sender, EventArgs e)
         {
             browser.Forward();
+        }
+        private void Browser_AddressChanged(object sender, AddressChangedEventArgs e)
+        {
+            var selectedBrowser = (ChromiumWebBrowser)sender;
+            this.Invoke(new MethodInvoker(() =>
+            {
+                toolStripAddressBar.Text = e.Address;
+            }));
+        }
+
+        private void toolStripButtonReload_Click(object sender, EventArgs e)
+        {
+            browser.Reload();
+        }
+
+        private void toolStripAddressBar_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                Navigate(toolStripAddressBar.Text);
+            }
+        }
+
+        private void Navigate(string address)
+        {
+            try
+            {
+                browser.Load(address);
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void toolStripButtonAddTab_Click(object sender, EventArgs e)
+        {
+            var newTabPage = new TabPage();
+            newTabPage.Text = "New Tab";
+            BrowserTabs.TabPages.Add(newTabPage);
         }
     }
 }
